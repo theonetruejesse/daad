0df47109b51ac1eb62837d661e94f840ff36669b
Your Name <the.jesse.dev@gmail.com>
2024-12-23 14:29:13 -0800

    cleaned up const naming

--- src/daad/clients/RabbitMQ/Client.py
+++ src/daad/clients/RabbitMQ/Client.py
@@ -5,7 +5,7 @@ import aio_pika
 
 from src.daad.clients.AppClient import AppClient
 from src.daad.clients.RabbitMQ.Broker import RabbitMQBroker
-from src.daad.constants import RABBITMQ_CONFIG, TEST_RABBITMQ_PROD, __prod__
+from src.daad.constants import IS_TEST_RABBITMQ_PROD, LOCAL_RABBITMQ_CONFIG, __prod__
 from src.daad.helpers import get_file_path
 
 
@@ -18,7 +18,7 @@ class RabbitMQClient(AppClient):
         self.broker = RabbitMQBroker()
 
     async def _setup(self):
-        if not __prod__ and not TEST_RABBITMQ_PROD:
+        if not __prod__ and not IS_TEST_RABBITMQ_PROD:
             self._start_local_rabbitmq()
 
         connection = await aio_pika.connect_robust(self._get_connection_string())
@@ -27,12 +27,13 @@ class RabbitMQClient(AppClient):
 
     def _start_local_rabbitmq(self):
         env = os.environ.copy()
+        config = LOCAL_RABBITMQ_CONFIG
         env.update(
             {
-                "RABBITMQ_DEFAULT_USER": RABBITMQ_CONFIG["user"],
-                "RABBITMQ_DEFAULT_PASS": RABBITMQ_CONFIG["password"],
-                "RABBITMQ_PORT": RABBITMQ_CONFIG["port"],
-                "RABBITMQ_MANAGEMENT_PORT": RABBITMQ_CONFIG["management_port"],
+                "RABBITMQ_DEFAULT_USER": config["user"],
+                "RABBITMQ_DEFAULT_PASS": config["password"],
+                "RABBITMQ_PORT": config["port"],
+                "RABBITMQ_MANAGEMENT_PORT": config["management_port"],
             }
         )
         subprocess.run(
@@ -40,10 +41,11 @@ class RabbitMQClient(AppClient):
         )
 
     def _get_connection_string(self):
+        config = LOCAL_RABBITMQ_CONFIG
         if __prod__:
             return os.getenv("RABBITMQ_URL")
         else:
-            return f"amqp://{RABBITMQ_CONFIG['user']}:{RABBITMQ_CONFIG['password']}@{RABBITMQ_CONFIG['host']}:{RABBITMQ_CONFIG['port']}/"
+            return f"amqp://{config['user']}:{config['password']}@{config['host']}:{config['port']}/"
 
     """ Broker Bindings """
 
--- src/daad/constants.py
+++ src/daad/constants.py
@@ -13,21 +13,12 @@ DAILY_LOG_CHANNEL = 1320122496363532310
 
 # RabbitMQ credentials
 
-TEST_RABBITMQ_PROD = False  # change this for when testing connections
+IS_TEST_RABBITMQ_PROD = False  # change this for when testing connections
 
-RABBITMQ_LOCAL = {
+LOCAL_RABBITMQ_CONFIG = {
     "user": "guest",
     "password": "guest",
     "host": "localhost",
     "port": "5672",
     "management_port": "15672",
 }
-
-RABBITMQ_PROD = {
-    "user": os.getenv("RABBITMQ_DEFAULT_USER"),
-    "password": os.getenv("RABBITMQ_DEFAULT_PASS"),
-    "url": os.getenv("RABBITMQ_URL"),
-    "private_url": os.getenv("RABBITMQ_PRIVATE_URL"),
-}
-
-RABBITMQ_CONFIG = RABBITMQ_PROD if TEST_RABBITMQ_PROD else RABBITMQ_LOCAL
